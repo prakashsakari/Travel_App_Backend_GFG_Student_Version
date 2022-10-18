9e9dcde27fdd4689ca2d42e4a4c9a136e92cfa7c
Prakash Sakari <80476561+prakashsakari@users.noreply.github.com>
2022-10-18 23:14:59 +0530

    initial commit

--- model/user.model.js
+++ model/user.model.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    number: { type: Number, required: true, uniquie: true },
-    email: { type: String, required: true, uniquie: true },
-    password: { type: String, required: true },
-})
+const userSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true },
+        number: { type: Number, required: true, uniquie: true },
+        email: { type: String, required: true, uniquie: true },
+        password: { type: String, required: true },
+    },
+    {
+        timestamps: true
+    }
+)
 
 
 const User = mongoose.model("User", userSchema);
