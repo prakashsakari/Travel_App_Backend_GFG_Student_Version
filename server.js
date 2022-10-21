1dbd3588f6caeebcac1b92194d22bdd287e81404
Prakash Sakari <80476561+prakashsakari@users.noreply.github.com>
2022-10-21 13:29:26 +0530

    fix: resolved cors issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -17,14 +18,15 @@ const connectDB = require("./config/dbconfig");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 connectDB();
 
 const PORT = 3500;
 
 app.get("/", (req, res) => {
-    res.send("Hello Geeks")
-})
+  res.send("Hello Geeks");
+});
 
 app.use("/api/hoteldata", hotelDataAddedToDBRouter);
 app.use("/api/categorydata", categoryDataAddedToDBRouter);
@@ -34,11 +36,9 @@ app.use("/api/hotels", singleHoterRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/wishlist", wishlistRouter);
 
-
 mongoose.connection.once("open", () => {
-    console.log("Connected to DB");
-    app.listen(process.env.PORT || PORT, () => {
-        console.log("Server is Up and Running")
-    })
-})
-
+  console.log("Connected to DB");
+  app.listen(process.env.PORT || PORT, () => {
+    console.log("Server is Up and Running");
+  });
+});
