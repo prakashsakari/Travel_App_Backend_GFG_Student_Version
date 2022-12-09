cdbc4e05ec817926dc96a21c504e6229e82f27c5
Prakash Sakari <80476561+prakashsakari@users.noreply.github.com>
2022-12-09 12:32:18 +0530

    added time stamp for user creation

--- model/user.model.js
+++ model/user.model.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
-        username: { type: String, required: true },
-        number: { type: Number, required: true, uniquie: true },
-        email: { type: String, required: true, uniquie: true },
-        password: { type: String, required: true },
-    },
-    {
-        timestamps: true
-    }
-)
-
+  {
+    username: { type: String, required: true },
+    number: { type: Number, required: true, uniquie: true },
+    email: { type: String, required: true, uniquie: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
